cc5cfd283f27ffb1847bf6c7a2835f50d7cc6128
JessicaNgo <jessica.ngo@live.com>
2019-01-26 23:41:27 -0800

    set skeleton and stubs for responding to get endpoints

--- main.py
+++ main.py
@@ -9,43 +9,62 @@ from flask import request
 
 app = Flask(__name__)
 #
-# keyword_map = {
-#     {"location":"vancouver"}, # where are you planning to go
-#     {"interest" : ["hiking", "snowboarding", "chocolate"]},
-#     {"time" : {"10:AM", "7:PM"}} # what do you like to do
-# }
+keyword_map = {
+    "Q1":{"location":"vancouver"}, # where are you planning to go
+    "Q2":{"interest" : ["hiking", "snowboarding", "chocolate"]},
+    {"time" : {"10:AM", "7:PM"}} # what do you like to do
+}
+
 
 def foodParser(*foods):
     return {}
 
 
 
-def create_querystring(*survey_answers):
-    querystring = defaultdict(str)
+def filter_results(response):
+    """ given a response object's json, filter out and select a few for itinerary
+    @param response: Response object containing json data from get request to yelp api
+    return: a list of dictionary where the list is a list of scheduled activities, and the dictionary contains information for each activity
+    """
+
+
+    return []
+
+def create_querystring(survey_answers):
+    """
+
+    :param survey_answers: a list of all survey answers as keywords to querystring
+    :return:
+    """
+    querystring = {}
     for ans in survey_answers:
-        keyword_map[ans]
+        querystring.update(ans)
+
+    return querystring
 
 
 @app.route('/')
 def main():
     print("this is req data", request.data)
-    print(request.get_json())
-    url = "https://api.yelp.com/v3/businesses/search"
-    location = ""
+    choices = request.get_json() # yelp keywords that we can use in query string
 
-    querystring = {"term":"asian","location":"vancouver"}
-    payload = ""
+    querystring = create_querystring(choices)
+    url = "https://api.yelp.com/v3/businesses/search"
+
+    # querystring = {"term":"asian,chinese","location":"vancouver","radius":7, "price":"1, 2, 3"}
     headers = {
     'Authorization': "Bearer TcTzRvNstk-8bdJkDiY3mwUhcCraFKD1SERukD6IxsOIiyN_pbbdmzb1JQdcCig0YYqECwWYEMB5YAD-8Z_XXso0MHOz47jJGqrH3S_t0L1V_frSvWq0jzsP1_5MXHYx",
     'cache-control': "no-cache",
     }
 
-    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    itinerary_objects = filter_results(response)
     #
-    # print(pprint(response.json()))
+    return itinerary_objects
+    # pprint(response.json())
     # print(type(response))
 
-    return str(["{}\n".format(line) for line in json.dumps(response.json()).split(",")])
+    # return str(["{}\n".format(line) for line in json.dumps(response.json()).split(",")])
 
 #
 #
